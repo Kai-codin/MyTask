f683887e5c4788134873bbe3db9246a7f3cd03c3
Kai <Kaiboucher2007@gmail.com>
2025-04-23 04:21:00 +0100

    V4

--- my-task/src/Login.jsx
+++ my-task/src/Login.jsx
@@ -29,7 +29,7 @@ function Login() {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const res = await fetch('https://mytask.kaicoding.cc:3000/api/login', {
+    const res = await fetch('http://localhost:3000/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       credentials: 'include',
